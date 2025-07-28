3caf0f28e251d30bbf47b626057380ff2815a7ad
Tuggi <paxtonphillips@outlook.com>
2025-07-28 13:03:41 -0700

    Add device.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './domain/prisma/prisma.service';
 import { ProjectModule } from './domain/project/project.module';
 import { ApiKeyModule } from './domain/api-key/api-key.module';
+import { DeviceModule } from './device/device.module';
 
 @Module({
-  imports: [ProjectModule, ApiKeyModule],
+  imports: [ProjectModule, ApiKeyModule, DeviceModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
